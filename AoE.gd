3abfd84ea217550795cfd2a5ff4960c676de84a8
neet5163 <g19911018@gmail.com>
2024-02-21 04:55:13 +0900

    exit追加

--- AoE.gd
+++ AoE.gd
@@ -1,18 +1,30 @@
 extends Area2D
+var hit_flag:bool = false
+@onready var n_visible_area:Node2D = $VisibleArea
+@onready var n_hit_area:CollisionShape2D = $HitArea
 
 func _ready() -> void:
 	# ビューポートのサイズを取得
 	var viewport_size: Vector2 = get_viewport_rect().size
 	# オブジェクトをビューポートの中央に配置
 	position = viewport_size / 2
+	# AoEの半径を取得
+	var aoe_radius = n_visible_area.radius
+	print("AoE Radius:", n_visible_area.radius)
+	# 
+	n_hit_area.shape.radius = aoe_radius
 
 func _on_timer_timeout() -> void:
-	 
-	queue_free()
+	hit_flag = true
+	#queue_free()
 	
 func _on_body_entered(body: Node2D) -> void:
 	print("on_body_entered!",body)
-	if body.name == "Me" and body.INVINCIBLE == false and body.can_hit == true:
+	if body.name == "Player" and body.INVINCIBLE == false and body.can_hit == true and hit_flag == true:
 		print("Hit!")
 		Audio.get_node("SeFail").play()
 		
+
+
+func _on_body_exited(body: Node2D) -> void:
+	print("on_body_exited!",body)
--- game_over.gd
+++ game_over.gd
@@ -7,31 +7,31 @@ enum{
 }
 
 # Line2Dの四角い枠が2つのボタンを囲う位置の配列
-var square_pose = [
+var square_pos = [
 	# Restart
 	Vector2(275, 350),
 	#Quit
-	Vector2(350, 350)
+	Vector2(382.5, 350)
 ]
 
 # enumで定義したパラメータを格納する
-var selected_option = null
+var selected_option:Variant
 # Restartボタンを押したときにConfirmationDialogに格納するテキスト
-var restart_text = "Do you really want to restart the game?"
+var restart_text:String = "Do you really want to restart the game?"
 # Quitボタン押したときにConfirmationDialogに格納するテキスト
-var quit_text = "Do you really want to quit the game?"
+var quit_text:String = "Do you really want to quit the game?"
 # RestartButtonのノード参照
-@onready var n_restart_btn = $VBoxContainer/ButtonsHBox/RestartVBox/RestartButton
+@onready var n_restart_btn:TextureButton = $VBoxContainer/ButtonsHBox/RestartVBox/RestartButton
 # QuitButtonのノード参照
-@onready var n_quit_btn = $VBoxContainer/ButtonsHBox/QuitVBox/QuitButton
+@onready var n_quit_btn:TextureButton = $VBoxContainer/ButtonsHBox/QuitVBox/QuitButton
 # ColorRectのノード参照
-@onready var n_color_rect = $ColorRect
+@onready var n_color_rect:ColorRect = $ColorRect
 # ConfirmationDialogのノード参照
-@onready var n_confirmation_dialog = $ConfirmationDialog
+@onready var n_confirmation_dialog:ConfirmationDialog = $ConfirmationDialog
 # Line2Dのノード参照
-@onready var n_line2D = $Line2D
+@onready var n_line2D:Line2D = $Line2D
 # AnimationPlayerのノード参照
-@onready var n_anim_player = $Line2D/AnimationPlayer
+@onready var n_anim_player:AnimationPlayer = $Line2D/AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -45,3 +45,90 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_restart_button_mouse_entered() -> void:
+	n_line2D.position = square_pos[0]
+	
+
+
+func _on_quit_button_mouse_entered() -> void:
+	n_line2D.position = square_pos[1]
+	
+
+
+func _on_restart_button_button_up() -> void:
+	work_at_button_up(RESTART)
+
+
+func _on_quit_button_button_up() -> void:
+	work_at_button_up(QUIT)
+
+
+# ボタンを押したときに呼ばれるメソッドを定義
+func work_at_button_up(option) -> void :
+	# 押したボタンに応じたenumパラメータを保持する引数optionを渡す
+	selected_option = option
+	# ColorRectノードを表示にして後ろを暗くする
+	n_color_rect.visible = true
+	# Restartボタンが押された場合
+	if selected_option == RESTART:
+		# ConfirmationDialogノードのTextプロパティにrestart_textの文字列を適用させる
+		n_confirmation_dialog.dialog_text = restart_text
+	# Quitボタンが押された場合
+	elif selected_option == QUIT:
+		# ConfirmationDialogノードのTextプロパティにquit_textの文字列を適用させる
+		n_confirmation_dialog.dialog_text = quit_text
+	# ConfirmationDialogを画面中央に表示
+	n_confirmation_dialog.popup_centered()
+
+
+func _on_confirmation_dialog_confirmed() -> void:
+	# Restartボタンを押した場合
+	if selected_option == RESTART:
+		# シーンツリーの一時停止を再開する
+		get_tree().paused = false
+		# デバッグ用
+		print("Scene tree paused: ", get_tree().paused)
+		# 指定シーンを読み込み直し、ゲームを始めから再開する
+		get_tree().change_scene_to_file("res://main.tscn")
+		# デバッグ用
+		print("The game is restarted.")
+	# Quitボタンを押した場合
+	elif selected_option == QUIT:
+		# ゲームを完全終了
+		get_tree().quit()
+		# デバッグ用
+		print("The game is quited.")
+
+
+func _on_confirmation_dialog_canceled() -> void:
+	# ボタン洗濯ステータスをリセット(値なし)
+	selected_option = null
+	# ConfirmationDialogノードのdealog_textプロパティのテキストを消す
+	n_confirmation_dialog.dialog_text = ""
+	# ColorRectノードを非表示に
+	n_color_rect.visible = false
+
+
+func _input(event: InputEvent) -> void:
+	# もしGameOverノードが表示されていたら(ゲームオーバーになったら)
+	if visible:
+		# もし ←キー が押されてキーから指が離れたら
+		if event.is_action_released("ui_left"):
+			# Line2Dの枠をRestart側に配置する
+			n_line2D.position = square_pos[0]
+		# もし →キー が押されてキーから指が離れたら
+		elif event.is_action_released("ui_right"):
+			# Line2Dの枠をQuit側に配置する
+			n_line2D.position = square_pos[1]
+		# もし spaceかenterが押されて指が離れて、かつColorRectが非表示なら
+		elif event.is_action_released("ui_accept") and not n_color_rect.visible:
+			# もしLine2Dの枠がRestart側に配置されていたら
+			if n_line2D.position == square_pos[0]:
+				# Restartボタンをクリックした時のメソッドを実行
+				_on_restart_button_button_up()
+			# もしLine2Dの枠がQuit側に配置されていたら
+			elif n_line2D.position == square_pos[1]:
+				# Quitボタンをクリックした時のメソッドを実行
+				_on_quit_button_button_up()
